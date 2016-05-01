c8fcaf545ef708acbd6d27c8019da4ce957adc8e
David Schreck <david.schreck@outlook.com>
2016-05-01 21:52:38 +0200

    Move Method and Conditional with Polymorphism

--- src/Price.java
+++ src/Price.java
@@ -1,4 +1,7 @@
 abstract class Price {
     abstract  int getPriceCode();
     abstract double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
