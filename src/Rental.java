0c4a13d83009eb877a6b46d9490a0544b4085995
David Schreck <david.schreck@outlook.com>
2016-05-01 21:33:54 +0200

    Move Method: getCharge and getFrequentRenterPoints

--- src/Rental.java
+++ src/Rental.java
@@ -1,5 +1,5 @@
 class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie newmovie, int newdaysRented) {
@@ -15,32 +15,12 @@ class Rental {
         return movie;
     }
 
-    double getCharge() {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    int getFrequentRenterPoints() {
+        return movie.getFrequentRenterPoints(daysRented);
     }
 
-    int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+    double getCharge() {
+        return movie.getCharge(daysRented);
     }
 
 }
